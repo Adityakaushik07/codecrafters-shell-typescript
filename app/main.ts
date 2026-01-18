5464d498dbc30af9539316bb723ef661f02c9762
Aditya Kaushik <kaushikaditya983@gmail.com>
2026-01-18 15:38:53 +0530

    Add EXIT command

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,20 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+function evaluate(input: string) {
+  if (input === "exit") return "EXIT";
+  return `${input}: command not found`;
+}
 function repl() {
   rl.question("$ ", (input: string) => {
-    console.log(`${input}: command not found`);
+    const result = evaluate(input);
+
+    if (result === "EXIT") {
+      rl.close();
+      return;
+    }
+
+    console.log(result);
     repl();
   });
 }
